e2f8efdf1b37cb88aa336f56b2f41d575348cfad
burabury <bura.bury@gmail.com>
2020-11-15 09:04:51 +0100

    Initial commit

---
+++ src/main/java/com/project/demo/ProjetkWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetkWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetkWithSpringApplication.class, args);
+    }
+
+}
